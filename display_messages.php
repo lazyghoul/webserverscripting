8fd6f2ca0068149f28483cce44beca70d0b42d7b
lazyghoul <josh.simpson@null.net>
2016-06-14 09:13:31 +0000

    Add files via upload

---
+++ display_messages.php
@@ -0,0 +1,143 @@
+<html>
+	<head>
+		<title>Web Scripting - Josh Simpson</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="alt">
+					<h1><a href="index.html">Web Scripting</a> by Josh Simpson</h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li>
+								<a href="#" class="icon fa-angle-down">Criteria</a>
+								<ul>
+                                    <li><a href="upload.html">Upload File</a></li>
+                                    <li><a href="#">Option Two</a></li>
+                                    <li><a href="#">Option Three</a></li>
+                                    <li><a href="#">Option Four</a></li>
+                                    <!---Removed, used for other pages
+									<li><a href="generic.html">Generic</a></li>
+									<li><a href="contact.html">Contact</a></li>
+									<li><a href="elements.html">Elements</a></li>
+                                    -------------------------------------------->
+                                    
+                                    <!---------- Removed, could be used if needed
+									<li>
+										<a href="#">Submenu</a>
+										<ul>
+											<li><a href="#">Option One</a></li>
+											<li><a href="#">Option Two</a></li>
+											<li><a href="#">Option Three</a></li>
+											<li><a href="#">Option Four</a></li>
+										</ul> 
+									</li>
+                                    --------------------------------------------->
+								</ul>
+							</li>
+							<li><a href="login.html" class="button">Sign In</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Main -->
+				<section id="main" class="container 75%">
+					<header>
+						<h2>Guest Book</h2>
+                        <h3>Messages</h3>
+					</header>
+					<div class="box">
+                        <?php
+
+                        //connect to MySQL
+                        $conn = @mysql_connect ("mysql.hostinger.co.uk", "u638900915_gbook", "gbookpassword") or die ("Sorry - unable to connect to database." );
+                    
+                        //select database
+                        $rs = @mysql_select_db ( "u638900915_gbook", $conn ) or die ( "error" );
+
+                        //create query
+                        $sql = "SELECT * FROM message";
+
+                        //execute query
+                        $rs = mysql_query( $sql,$conn );
+
+                        //display results
+                        echo ( "<table border='1'><tr><td><b>Name</b></td><td><b>Message</b></td></tr>" );
+
+                        while ( $row = mysql_fetch_array( $rs ) )
+                        {
+                            echo  ( "<tr><td>".$row["guestName"]."</td><td>".$row["msg"]."</td></tr>" );  
+                        }
+                        echo ( "</table>" );
+
+                        ?>
+                        <p>Click <a href="guestbook.html">here</a> to return to Guestbook.</p>
+                        
+                        
+						<!---------------- Contact form, unneccesary------------------------------
+                        <form method="post" action="#">
+							<div class="row uniform 50%">
+								<div class="6u 12u(mobilep)">
+									<input type="text" name="name" id="name" value="" placeholder="Name" />
+								</div>
+								<div class="6u 12u(mobilep)">
+									<input type="email" name="email" id="email" value="" placeholder="Email" />
+								</div>
+							</div>
+							<div class="row uniform 50%">
+								<div class="12u">
+									<input type="text" name="subject" id="subject" value="" placeholder="Subject" />
+								</div>
+							</div>
+							<div class="row uniform 50%">
+								<div class="12u">
+									<textarea name="message" id="message" placeholder="Enter your message" rows="6"></textarea>
+								</div>
+							</div>
+							<div class="row uniform">
+								<div class="12u">
+									<ul class="actions align-center">
+										<li><input type="submit" value="Send Message" /></li>
+									</ul>
+								</div>
+							</div>
+						</form>
+                        ----------------------------------------------------------------------->
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
